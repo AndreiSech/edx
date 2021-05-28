6079da6f321f764e7be56b7f226352fa93ff56cb
a.sech <a.sech@foranx.by>
2021-05-28 16:30:32 +0300

    upd py hello

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ greeting = "Hello, "
 name = input("Name: ")
 print(greeting + name)
 print(f"Nice to meet you {name}")
+
+n = int(input("Number: "))
+
+if n > 0:
+    print(f"number {n} is nositive")
+elif n == 0:
+    print(f"number is EQUALS 0")
+else:
+    print(f"number {n} is negative")
